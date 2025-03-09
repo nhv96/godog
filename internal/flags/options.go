e0ebe4151dcf70d5885a263fdaa2f41d7717199a
VinhNguyenHoang <vinhnguyenhoang96@gmail.com>
2025-03-09 22:46:14 +0700

    rename wording

--- internal/flags/options.go
+++ internal/flags/options.go
@@ -81,7 +81,7 @@ type Options struct {
 	// ShowHelp enables suite to show CLI flags usage help and exit.
 	ShowHelp bool
 
-	// MaxRetries is used to retry the number of time when a scenario failed. Default is 0 retry.
+	// MaxRetries is used to retry the number of time when a step is failed. Default is 0 retry.
 	MaxRetries int
 }
 
