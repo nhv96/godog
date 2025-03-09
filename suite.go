352feb78953f8a0082be83d23d6e6cdffccf3981
VinhNguyenHoang <vinhnguyenhoang96@gmail.com>
2025-03-09 22:45:16 +0700

    handle err without printing retry steps

--- suite.go
+++ suite.go
@@ -218,6 +218,23 @@ func (s *suite) runStep(ctx context.Context, pickle *Scenario, step *Step, scena
 			sr := models.NewStepResult(models.Ambiguous, pickle.Id, step.Id, match, pickledAttachments, err)
 			s.storage.MustInsertPickleStepResult(sr)
 			s.fmt.Ambiguous(pickle, step, match.GetInternalStepDefinition(), err)
+		case errors.Is(err, ErrRetry):
+			// retrieve the step result if exist,
+			// if the step has used all retry attempts, mark it as failed,
+			// otherwise store a new step result record.
+			var sr models.PickleStepResult
+			sr = s.storage.GetPickleStepResult(step.Id)
+			if sr.PickleID == pickle.Id && sr.PickleStepID == step.Id {
+				if sr.RunAttempt >= s.maxRetries {
+					sr.Status = models.Failed
+					s.fmt.Failed(pickle, step, match.GetInternalStepDefinition(), err)
+				} else {
+					sr.RunAttempt++
+				}
+			} else {
+				sr = models.NewStepResult(models.Retry, pickle.Id, step.Id, match, pickledAttachments, err)
+			}
+			s.storage.MustUpsertPickleStepResult(sr)
 		default:
 			sr := models.NewStepResult(models.Failed, pickle.Id, step.Id, match, pickledAttachments, err)
 			s.storage.MustInsertPickleStepResult(sr)
